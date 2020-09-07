7a641dfd30dd928b760b2c6a51689b118684d3be
Óscar Lamelas Damas <lamelas84@gmail.com>
2020-09-07 23:58:05 +0200

    work in progress

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-const { match, pipe, add, length, indexOf } = require("ramda");
+const { match, pipe, add, length, indexOf, split } = require("ramda");
+const status = {
+  RAISIN: "o",
+  NOT_RAISIN: ".",
+  USED: "x",
+}
 
 const countRaisins = pipe(match(/o/g), length);
 const width = indexOf("\n");
 const height = pipe(match(/\n/g), length, add(1));
 
-const divisors = (n) =>
+const divisorsOf = (n) =>
   [...Array(n + 1).keys()].slice(1).filter((i) => n % i === 0);
+
+const getPossibleShapesOfArea = (area) => {
+  const divisors = divisorsOf(area);
+
+  return divisors.map((n) => ({
+    width: n,
+    height: area / n,
+  }));
+};
+
+const cakeArray = split(/\n/g);
+
+module.exports = {
+  divisorsOf,
+  getPossibleShapesOfArea,
+  cakeArray,
+  status,
+};
 
 function cut(cake) {
   const raisins = countRaisins(cake);
@@ -15,8 +38,6 @@ function cut(cake) {
 
   console.log(raisins, w, h, area);
 }
-
-exports.divisors = divisors;
 
 const cake = `........
 ..o.....
--- test.js
+++ test.js
@@ -1,9 +1,53 @@
-const { divisors } = require("./index");
+const { divisorsOf, getPossibleShapesOfArea, cakeArray } = require("./index");
+describe("divisorsOf tests", () => {
+  test("Divisors of 4", () => {
+    expect(divisorsOf(4)).toStrictEqual([1, 2, 4]);
+  });
 
-test("Divisors of 4", () => {
-  expect(divisors(4)).toStrictEqual([1, 2, 4]);
+  test("Divisors of 10", () => {
+    expect(divisorsOf(10)).toStrictEqual([1, 2, 5, 10]);
+  });
 });
 
-test("Divisors of 10", () => {
-  expect(divisors(10)).toStrictEqual([1, 2, 5, 10]);
+describe("getPossibleShapesOfArea tests", () => {
+  test("Area 1", () => {
+    expect(getPossibleShapesOfArea(1)).toStrictEqual([{ width: 1, height: 1 }]);
+  });
+
+  test("Area 2", () => {
+    expect(getPossibleShapesOfArea(2)).toStrictEqual([
+      { width: 1, height: 2 },
+      { width: 2, height: 1 },
+    ]);
+  });
+
+  test("Area 12", () => {
+    expect(getPossibleShapesOfArea(12)).toStrictEqual([
+      { width: 1, height: 12 },
+      { width: 2, height: 6 },
+      { width: 3, height: 4 },
+      { width: 4, height: 3 },
+      { width: 6, height: 2 },
+      { width: 12, height: 1 },
+    ]);
+  });
+});
+
+describe("cakeArray tests", () => {
+  test("Basic test", () => {
+    const cake = `.o.`;
+
+    const actual = cakeArray(cake);
+
+    expect(actual[0]).toStrictEqual(cake);
+  });
+
+  test("Multiline test", () => {
+    const cake = `.o.\no.o`;
+    const expected = [".o.", "o.o"];
+
+    const actual = cakeArray(cake);
+
+    expect(actual).toStrictEqual(expected);
+  });
 });
